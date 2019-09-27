d3f16b80d41ba1475af240fb6f3961f3fed52d53
niki9796dk <nikiewald@hotmail.dk>
2019-09-27 11:53:58 +0200

    Added speed to image

---
+++ source/numpyNumbers.py
@@ -0,0 +1,136 @@
+import numpy as np
+
+
+class NumpyNumbers:
+    number_size = (7, 5)
+    number_color = (255, 255, 50)  # (B, G, R) Cyan
+
+    def getOverlay(self, number: int, width: int, height: int):
+        overlay = self._getNumber(number)
+
+        overlay_height = overlay.shape[0]
+        overlay_width = overlay.shape[1]
+        missing_width = width - overlay_width
+
+        overlay = np.concatenate((overlay, np.zeros((overlay_height, missing_width, 3))), axis=1)
+        overlay = np.concatenate((overlay, np.zeros((height - overlay_height, width, 3))), axis=0)
+
+        return overlay
+
+    def _getNumber(self, number: int):
+        number_str = str(number)
+        numbers = None
+
+        for num in number_str:
+            numpy = self._getImageNumber(int(num))
+
+            if numbers is None:
+                numbers = numpy
+            else:
+                numbers = np.concatenate((numbers, np.zeros((self.number_size[0], 1, 3)), numpy), axis=1)
+
+        return numbers
+
+    def _getImageNumber(self, number: int):
+        num = np.array(self.numbers[number])
+        return self._addColorChannels(num)
+
+    def _addColorChannels(self, number: np):
+        return np.dstack((
+            number * self.number_color[0],  # B
+            number * self.number_color[1],  # G
+            number * self.number_color[2],  # R
+        ))
+
+    numbers = [
+        [
+            [0, 1, 1, 1, 0],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 1, 1, 0],
+        ],
+        [
+            [0, 0, 1, 0, 0],
+            [0, 1, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [1, 1, 1, 1, 1],
+        ],
+        [
+            [0, 1, 1, 1, 0],
+            [1, 0, 0, 0, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0],
+            [1, 1, 1, 1, 1],
+        ],
+        [
+            [0, 1, 1, 1, 0],
+            [1, 0, 0, 0, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 1, 1, 0],
+        ],
+        [
+            [0, 0, 0, 1, 0],
+            [0, 0, 1, 1, 0],
+            [0, 1, 0, 1, 0],
+            [1, 0, 0, 1, 0],
+            [1, 1, 1, 1, 1],
+            [0, 0, 0, 1, 0],
+            [0, 0, 0, 1, 0],
+        ],
+        [
+            [1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 1, 1, 0],
+        ],
+        [
+            [0, 1, 1, 1, 0],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 1, 1, 0],
+        ],
+        [
+            [1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+        ],
+        [
+            [0, 1, 1, 1, 0],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 1, 1, 0],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 1, 1, 0],
+        ],
+        [
+            [0, 1, 1, 1, 0],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 1, 1, 0],
+        ]
+    ]
