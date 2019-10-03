cdd2cb36611dcbcb2bf6be926369dad68493d066
Simon Steiner <Ssimon17@student.aau.dk>
2019-10-03 09:07:20 +0200

    Made the model name dynamically generated Gave more turn sensi anti reward

--- gym_carla/settings.py
+++ gym_carla/settings.py
@@ -3,10 +3,11 @@ AGENT_SYNCED = True  # Synchronizes agent with frame updates from Carla
 AGENT_TIME_STEP_SIZE = 0.1  # Size of fixed time step
 
 # CarlaEnv settings
-CARLA_PATH = '/home/d506e19/carla/Dist/CARLA_Shipping_0.9.6-23-gc6624e4c-dirty/LinuxNoEditor'  # Path to Carla root folder
+CARLA_PATH = '/home/d506e19/carla/Dist/CARLA_Shipping_0.9.6-25-gf909d022-dirty/LinuxNoEditor'  # Path to Carla root folder
 CARLA_SIMS_NO = 4  # Number of simulations
 CARLA_SIMS = [['localhost', 3000, 'Curve2'], ['localhost', 3003, 'Curve2'], ['localhost', 3006, 'Curve2'], ['localhost', 3009, 'Curve2'], ['localhost', 3012, 'Curve2'], ['localhost', 3015, 'Curve2']]  # Possible simulations that can be created in format [Host, port, mapname]
-CARLA_TICKS_PER_EPISODE = 45 * (1 / AGENT_TIME_STEP_SIZE)  # Steps
+CARLA_SECONDS_PER_EPISODE = 45
+CARLA_TICKS_PER_EPISODE = CARLA_SECONDS_PER_EPISODE * (1 / AGENT_TIME_STEP_SIZE)  # Steps
 CARLA_IMG_WIDTH = 50  # Pixels
 CARLA_IMG_HEIGHT = 50  # Pixels
 CARLA_IMG_MAX_SPEED = 80  # km/h
@@ -20,9 +21,8 @@ VIDEO_ALWAYS_ON = False  # Bool
 # Model settings
 MODEL_RL_MODULE = "PPO2"
 MODEL_POLICY = "CnnLstmPolicy"
-MODEL_NAME = "PPO2_Reproduce_moreTimeSteps_MD_MorePenalty_turnSensi"
-MODEL_NUMBER = 9  # int|None. If int carla will try to load model, if none it will never even try!
+MODEL_NUMBER = 32  # int|None. If int carla will try to load model, if none it will never even try!
 MODEL_USE_TENSORBOARD_LOG = True  # Bool
 MODEL_EXPORT_RATE = 100 if MODEL_RL_MODULE is "PPO2" else 2000  # Callback steps (Dependent on n_steps of rl_module, ppo2=128, a2c=5)
 MODEL_N_STEPS = 256
-MODEL_ACTION_TYPE = 1  # 0: Discrete, 1: MultiDiscrete, 2: Box
+MODEL_ACTION_TYPE = 2  # 0: Discrete, 1: MultiDiscrete, 2: Box
