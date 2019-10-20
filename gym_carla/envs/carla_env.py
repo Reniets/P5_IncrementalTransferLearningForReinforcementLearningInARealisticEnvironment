Changed model settings (Hopefully) Fixed error that occured after long training on same map (Integer overflow)

gym_carla/envs/carla_env.py
@@ -1,19 +1,18 @@
 import math
 import os
 import queue
+import random
+
 import gym
 from gym.spaces import Discrete, Box, Tuple, MultiDiscrete
 from database.sql import Sql
 import numpy as np
 from gym_carla.carla_utils import *
 import cv2
-
 # Import classes
-from source.media_handler import MediaHandler
 from source.reward import Reward
-# from source.media_handler import MediaHandler
-
-
+from source.media_handler import MediaHandler
+from multiprocessing import Condition, Lock
 makeCarlaImportable()
 import carla
 from PIL import Image
@@ -21,12 +20,13 @@ from PIL import Image
 
 class CarlaEnv(gym.Env):
     """Sets up CARLA simulation and declares necessary instance variables"""
-    def __init__(self, name="NoNameWasGiven", carlaInstance=0):
 
+    def __init__(self, carlaInstance=0, world_ticks=None, name="NoNameWasGiven", serverIndex=0):
         # Connect a client
-        self.client = carla.Client(*settings.CARLA_SIMS[0][:2])
+        self.client = carla.Client(*settings.CARLA_SIMS[serverIndex][:2])
         self.client.set_timeout(2.0)
         self.modelName = name
+        self.world_ticks = world_ticks
 
         # Set necessary instance variables related to client
         self.world = self.client.get_world()
@@ -40,8 +40,10 @@ class CarlaEnv(gym.Env):
         self.episodeTicks = 0
         self.totalTicks = 0
 
+        self.frameNumber = 0
+
         # Video variables
-        self.episodeNr = 0
+        self.episodeNr = 0  # TODO WARNING: be careful using this as it also counts validation episodes
         self.sql = Sql()
         self.sessionId = self.sql.INSERT_newSession(self.modelName) if (self.carlaInstance == 0) else None
 
@@ -53,13 +55,16 @@ class CarlaEnv(gym.Env):
         self.vehicle = None
         self.segSensor = None
         self.grassSensor = None
+        self.splineSensor = None
         self.imgFrame = None
         self.wheelsOnGrass = None
         self.episodeStartTime = 0
         self.episodeReward = None
-        self.frameNumber = 0
+        self.distanceOnSpline = None
+        self.splineMaxDistance = None
+        self.previousDistanceOnSpline = None
+
         self.queues = []  # List of tuples (queue, dataProcessingFunction)
-        self.lastAction = None
 
         # Declare reward dependent values
         self.car_last_tick_pos = None
@@ -82,26 +87,29 @@ class CarlaEnv(gym.Env):
         elif settings.MODEL_ACTION_TYPE == ActionType.MULTI_DISCRETE.value:
             # 1) Throttle: Discrete 4 - [0]:0.0, [1]:0.3, [2]:0.6, [3]:1.0
             # 2) Brake: Discrete 3 - [0]:0.0, [1]:0.5, [2]:1
-            # 3) Steer: Discrete 5 - [0]:-1.0, [1]:-0.3, [2]:-0.6, [3]:0.0, [4]:0.3, [5]:0.6, [6]:1.0
-            self.action_space = MultiDiscrete([4, 3, 7])
+            # 3) Steer: Discrete 5 - [0]:-1.0, [1]:-0.5, [2]:0.0, [3]:0.5, [4]:1.0
+            self.action_space = MultiDiscrete([4, 3, 21])
             self.throttleMapLen = float(self.action_space.nvec[0]-1)
             self.brakeMapLen = float(self.action_space.nvec[1]-1)
             self.steerMapLen = float(self.action_space.nvec[2]-1)/2
         elif settings.MODEL_ACTION_TYPE == ActionType.BOX.value:
             # [Throttle, Steer, brake]
-            self.action_space = Box(np.array([0, 0, -1]), np.array([+1, +1, +1]), dtype=np.float32)
+            self.action_space = Box(np.array([0, 0, -0.5]), np.array([+1, +1, +0.5]), dtype=np.float32)
         else:
             raise Exception("No such action type, change settings")
 
         if settings.AGENT_SYNCED: self.tick(30)
 
+    def close(self):
+        self._resetActorList()
+
     ''':returns initial observation'''
     def reset(self):
-        self.episodeNr += 1  # Count episodes
+        self.episodeNr += 1  # Count episodes TODO WARNING: be careful using this as it also counts validation episodes
 
         # Print episode and reward for that episode
         if self.carlaInstance == 0 and self.car_last_episode_time is not None:
-            print(f"Episode: {self.episodeNr} - Reward: {self.episodeReward} \t - Time: {time.time() - self.car_last_episode_time}")
+            print(f"Episode:  {self.episodeNr} - Reward: {self.episodeReward} \t - Time: {time.time() - self.car_last_episode_time}")
 
         # Frames are only added, if it's a video episode, so if there are frames it means that last episode
         # was a video episode, so we should export it, before we reset the frames list below
@@ -129,13 +137,12 @@ class CarlaEnv(gym.Env):
 
         # Disengage brakes from earlier workaround
         self._applyActionDiscrete(Action.DO_NOTHING.value)
-
         return self.imgFrame  # Returns initial observation (First image)
 
     ''':returns (obs, reward, done, extra)'''
     def step(self, action):
         self.episodeTicks += 1
-        #self.totalTicks += 1
+        # self.totalTicks += 1
 
         # Do action
         if settings.MODEL_ACTION_TYPE == ActionType.DISCRETE.value:
@@ -147,7 +154,8 @@ class CarlaEnv(gym.Env):
         else:
             raise Exception("No such action type, change settings")
 
-        if settings.AGENT_SYNCED: self.tick(30)
+        if settings.AGENT_SYNCED:
+            self.tick(60)
 
         is_done = self._isDone()  # Must be calculated before rewards
 
@@ -163,9 +171,17 @@ class CarlaEnv(gym.Env):
         return self.imgFrame, reward, is_done, {}  # extra
 
     def tick(self, timeout):
-        self.frameNumber = self.world.tick()
+        self.world.tick()
+
         data = [self._retrieve_data(queueTuple, timeout) for queueTuple in self.queues]
-        assert all(x.frame == self.frameNumber for x in data)
+        # assert all(x.frame == self.frameNumber.value for x in data)
+        return data
+
+    def tick_unsync(self, timeout):
+        self.frameNumber.value = self.world.tick()
+
+        data = [self._retrieve_data(queueTuple, timeout) for queueTuple in self.queues]
+        # assert all(x.frame == self.frameNumber.value for x in data)
         return data
 
     def _makeQueue(self, registerEvent, processData):
@@ -178,7 +194,7 @@ class CarlaEnv(gym.Env):
             data = queueTuple[0].get(timeout=timeout)
             dataProcessFunction = queueTuple[1]
 
-            if data.frame == self.frameNumber:
+            if data.frame == self.frameNumber.value:
                 dataProcessFunction(data)  # Process data
                 return data
 
@@ -192,7 +208,6 @@ class CarlaEnv(gym.Env):
         self.episodeTicks = 0
         self.episodeReward = None
         self.queues = []
-        self.lastAction = None
 
         # Early stopping
         self.grassLocation = None
@@ -203,6 +218,7 @@ class CarlaEnv(gym.Env):
         self.car_last_tick_transform = None
         self.car_last_tick_wheels_on_road = None
         self.car_last_episode_time = time.time()
+        self.previousDistanceOnSpline = None
 
         # Video
         self.mediaHandler.episodeFrames = []
@@ -210,6 +226,10 @@ class CarlaEnv(gym.Env):
     def _createActors(self):
         # Spawn vehicle
         self.vehicle = self._createNewVehicle()
+        #print(self.vehicle.attributes)
+        #self.vehicle.attributes['color'] = '255,0,0'
+        #print(self.vehicle.attributes)
+
         self.actorList.append(self.vehicle)  # Add to list of actors which makes it easy to clean up later
 
         # Make segmentation sensor blueprint
@@ -219,6 +239,9 @@ class CarlaEnv(gym.Env):
         # Create grass sensor
         self.grassSensor = self._createGrassSensor()
         self.actorList.append(self.grassSensor)
+
+        self.splineSensor = self._createSplineSensor()
+        self.actorList.append(self.splineSensor)
 
     # Destroy all previous actors, and clear actor list
     def _resetActorList(self):
@@ -231,9 +254,12 @@ class CarlaEnv(gym.Env):
 
     # Waits until the world is ready for training
     def _waitForWorldToBeReady(self):
-        self.world.tick()
+        self.tick_lock.acquire()
         while self._isWorldNotReady():
-            if settings.AGENT_SYNCED: self.tick(10)
+            if settings.AGENT_SYNCED: self.tick_unsync(60)
+        self.tick_lock.release()
+
+        self.tick(30)
 
     # Returns true if the world is not yet ready for training
     def _isWorldNotReady(self):
@@ -244,7 +270,14 @@ class CarlaEnv(gym.Env):
     # Returns the vehicle
     def _createNewVehicle(self):
         vehicle_blueprint = self.blueprintLibrary.filter('test')[0]
-        vehicle_spawn_transform = self.world.get_map().get_spawn_points()[0]  # Pick first (and probably only) spawn point
+        color = random.choice(vehicle_blueprint.get_attribute('color').recommended_values)
+        vehicle_blueprint.set_attribute('color', '0,255,0')
+
+        vehicle_spawn_transforms = self.world.get_map().get_spawn_points()
+        if settings.USE_RANDOM_SPAWN_POINTS:
+            vehicle_spawn_transform = random.choice(vehicle_spawn_transforms)  # Pick a random spawn point
+        else:
+            vehicle_spawn_transform = vehicle_spawn_transforms[0]  # Use the first spawn point
         return self.world.spawn_actor(vehicle_blueprint, vehicle_spawn_transform)  # Spawn vehicle
 
     # Creates a new segmentation sensor and spawns it into the world as an actor
@@ -278,14 +311,25 @@ class CarlaEnv(gym.Env):
         # Return created actor
         return grass_sensor
 
+    # Creates a new spline distance sensor and spawns it into the world as an actor
+    # Returns the sensor
+    def _createSplineSensor(self):
+        # Sensor blueprint
+        spline_blueprint = self.blueprintLibrary.find('sensor.other.spline_distance')
+
+        # Grass sensor actor
+        spline_sensor = self.world.spawn_actor(spline_blueprint, carla.Transform(), attach_to=self.vehicle)
+        self._makeQueue(spline_sensor.listen, processData=self._spline_data)
+        # Return created actor
+        return spline_sensor
+
     # Applies a discrete action to the vehicle
     def _applyActionDiscrete(self, action):
         # If action does something, apply action
         self.vehicle.apply_control(carla.VehicleControl(
             throttle=DISCRETE_ACTIONS[Action(action)][0],
             brake=DISCRETE_ACTIONS[Action(action)][1],
-            steer=DISCRETE_ACTIONS[Action(action)][2],
-            manual_gear_shift=False
+            steer=DISCRETE_ACTIONS[Action(action)][2]
         ))
 
     def _applyActionMultiDiscrete(self, action):
@@ -293,8 +337,7 @@ class CarlaEnv(gym.Env):
         self.vehicle.apply_control(carla.VehicleControl(
             throttle=action[0]/self.throttleMapLen,
             brake=action[1]/self.brakeMapLen,
-            steer=(action[2]/self.steerMapLen)-1,
-            manual_gear_shift=False
+            steer=(action[2]/self.steerMapLen)-1
         ))
 
     # Applies a box action to the vehicle
@@ -303,7 +346,6 @@ class CarlaEnv(gym.Env):
             throttle=float(action[0]),
             brake=float(action[1]),
             steer=float(action[2]),
-            manual_gear_shift=False
         ))
 
     # Returns the amount of meters traveled since last tick
@@ -337,6 +379,16 @@ class CarlaEnv(gym.Env):
 
         return kph
 
+    def getDistanceMovedAlongSpline(self):
+        distanceAlongSpline = self.distanceOnSpline - self.previousDistanceOnSpline if self.previousDistanceOnSpline is not None else 0
+        if distanceAlongSpline < -0.8 * self.splineMaxDistance:  # If the car has completed an entire loop the distance moved will be a negative number close to the max spline distance
+            distanceAlongSpline = self.splineMaxDistance - self.previousDistanceOnSpline + self.distanceOnSpline  # Should instead be the distance to the finish line + the distance past the finish line
+        elif distanceAlongSpline > 0.8 * self.splineMaxDistance:  # If the car somehow reverses by the finish line it will have moved a distance close to max spline distance
+            distanceAlongSpline = -(self.previousDistanceOnSpline + (self.splineMaxDistance - self.distanceOnSpline))  # Should instead be the negative distance that the vehicle moved backwards
+        elif abs(distanceAlongSpline) > 1000:
+            distanceAlongSpline = 0
+        return distanceAlongSpline/100
+
     # Returns true if the current episode should be stopped
     def _isDone(self):
         # If episode length is exceeded it is done
@@ -345,11 +397,23 @@ class CarlaEnv(gym.Env):
         car_on_grass = self._isCarOnGrass()
         max_negative_reward = self._isMaxNegativeRewardAccumulated()
 
-        return episode_expired or is_stuck_on_grass  # or car_on_grass or max_negative_reward
+        return episode_expired #or is_stuck_on_grass  # or car_on_grass or max_negative_reward
 
     # Returns true if the current max episode time has elapsed
     def _isEpisodeExpired(self):
-        return self.episodeTicks > settings.CARLA_TICKS_PER_EPISODE_STATIC
+        if settings.CARLA_SECONDS_MODE_LINEAR:
+            scale = min((self.episodeNr / settings.CARLA_SECONDS_PER_EPISODE_EPISODE_RANGE), 1)                         # Calculate scale depending on episode nr
+            range_diff = settings.CARLA_SECONDS_PER_EPISODE_LINEAR_MAX - settings.CARLA_SECONDS_PER_EPISODE_LINEAR_MIN  # Calculate min / max difference
+            total_seconds = settings.CARLA_SECONDS_PER_EPISODE_LINEAR_MIN + (range_diff * scale)                        # Calculate current total episodes
+
+            # if self.carlaInstance == 0:
+            #     print(str(total_seconds))
+
+            total_max_episode_ticks = int(total_seconds * (1 / settings.AGENT_TIME_STEP_SIZE))                          # Calculate new total_max_episode_ticks
+        else:
+            total_max_episode_ticks = settings.CARLA_TICKS_PER_EPISODE_STATIC
+
+        return self.episodeTicks > total_max_episode_ticks
 
     # Returns true if all four wheels are not on the road
     def _isCarOnGrass(self):
@@ -375,3 +439,8 @@ class CarlaEnv(gym.Env):
         self.wheelsOnGrass = event[0] + event[1] + event[2] + event[3]
         # print(f"({event[0]},{event[1]},{event[2]},{event[3]})")
 
+    def _spline_data(self, event):
+        self.distanceOnSpline = event[0]
+        self.splineMaxDistance = event[1]
+
+        # if self.carlaInstance is 0 and (self.episodeTicks + 1) % 100 == 0: print(f"Progress: {self.distanceOnSpline/self.splineMaxDistance*100:.2f}%")

source/runner.py
@@ -21,7 +21,7 @@ from source.reward import Reward
 import numpy as np
 import tensorflow as tf
 from multiprocessing import Condition, Value
-from ctypes import c_int
+from ctypes import c_uint64
 
 
 class Runner:
@@ -35,8 +35,8 @@ class Runner:
         self.model = None
         self.maxRewardAchieved = float('-inf')
         self.lock = Condition(Lock())
-        self.frameNumber = Value(c_int, 0)
-        self.waiting_threads = Value(c_int, 0)
+        self.frameNumber = Value(c_uint64, 0)
+        self.waiting_threads = Value(c_uint64, 0)
         self.prev_episode = 0
         self.modelName = None
 
@@ -81,9 +81,9 @@ class Runner:
 
     def _setup(self):
         # Setup environment
-        startCarlaSims()
+        frame = startCarlaSims()
 
-        self.world_ticks = Value(c_int, 0)
+        self.frameNumber = Value(c_uint64, frame)
 
         # self.env = SubprocVecEnv([lambda i=i: gym.make('CarlaGym-v0', name=self.modelName, carlaInstance=i) for i in range(settings.CARLA_SIMS_NO)])
         self.env = SubprocVecEnv([self.make_env(i) for i in range(settings.CARS_PER_SIM)])
@@ -215,9 +215,9 @@ class Runner:
         tensorboard_log = "./ExperimentTensorboardLog" if settings.MODEL_USE_TENSORBOARD_LOG else None
 
         # Load from previous model:
-        if settings.MODEL_NUMBER is not None and os.path.isfile(f"ExperimentLogsFinal/{self.modelName}_{self.modelNum}_best.pkl"):
+        if settings.MODEL_NUMBER is not None and os.path.isfile(f"ExperimentLogsFinal/{self.modelName}_{self.modelNum}.pkl"):
             print("LOAD MODEL")
-            model = self.rlModule.load(f"ExperimentLogsFinal/{self.modelName}_{self.modelNum}_best.pkl", env=self.env, tensorboard_log=tensorboard_log, n_steps=settings.MODEL_N_STEPS, nminibatches=settings.MODEL_MINI_BATCHES, ent_coef=settings.MODEL_ENT_COEF, learning_rate=lambda frac: settings.MODEL_LEARNING_RATE, cliprange=lambda frac: settings.MODEL_CLIP_RANGE, cliprange_vf=lambda frac: settings.MODEL_CLIP_RANGE)
+            model = self.rlModule.load(f"ExperimentLogsFinal/{self.modelName}_{self.modelNum}.pkl", env=self.env, tensorboard_log=tensorboard_log, n_steps=settings.MODEL_N_STEPS, nminibatches=settings.MODEL_MINI_BATCHES, ent_coef=settings.MODEL_ENT_COEF, learning_rate=lambda frac: settings.MODEL_LEARNING_RATE, cliprange=lambda frac: settings.MODEL_CLIP_RANGE, cliprange_vf=lambda frac: settings.MODEL_CLIP_RANGE)
             print("done loading")
             self.modelNum += 1  # Avoid overwriting the loaded model
         # Create new model
@@ -236,17 +236,29 @@ class Runner:
 
         # TODO: If we want to evaluate on 'alt' maps, load them here!!!
 
-        obs = self.env.reset()
+        # obs = self.env.reset()
+        #
+        # state = None
+        # # When using VecEnv, done is a vector
+        # done = [False for _ in range(self.env.num_envs)]
+        # rewards_accum = np.zeros(settings.CARS_PER_SIM)
+        # for _ in range(settings.CARLA_TICKS_PER_EPISODE_STATIC):
+        #     # We need to pass the previous state and a mask for recurrent policies
+        #     # to reset lstm state when a new episode begin
+        #     action, state = self.model.predict(obs, state=state, mask=done, deterministic=False)
+        #     obs, rewards, done, _ = self.env.step(action)
+        #     rewards_accum += rewards
+
+        rewards_accum = np.zeros(settings.CARS_PER_SIM)
 
         state = None
-        # When using VecEnv, done is a vector
-        done = [False for _ in range(self.env.num_envs)]
-        rewards_accum = np.zeros(settings.CARS_PER_SIM)
-        for _ in range(settings.CARLA_TICKS_PER_EPISODE_STATIC):
-            # We need to pass the previous state and a mask for recurrent policies
-            # to reset lstm state when a new episode begin
+        done = False
+        obs = self.env.reset()
+
+        # Play some limited number of steps
+        for i in range(settings.CARLA_TICKS_PER_EPISODE_STATIC):
             action, state = self.model.predict(obs, state=state, mask=done, deterministic=True)
-            obs, rewards, done, _ = self.env.step(action)
+            obs, rewards, done, info = self.env.step(action)
             rewards_accum += rewards
 
         self.env.reset()