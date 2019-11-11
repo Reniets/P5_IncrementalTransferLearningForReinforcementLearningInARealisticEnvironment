d3ef3c5ac1b4d0a9268483b9943aa0ae49a63134
Simon <ssimon17@student.aau.dk>
2019-11-11 13:10:03 +0100

    Small changes

--- source/data_plotting/graphData.py
+++ source/data_plotting/graphData.py
@@ -10,11 +10,16 @@ sessions = {
         'Base_4':           1063,
         'Base_5':           1064,
         'Base_6':           1069,
-        'Transfer_1-2':     1071,
-        'Transfer_2-3':     1072,
-        'Transfer_3-4':     1073,
-        'Transfer_4-5':     1074,
-        'Transfer_5-6':     1081
+        'Transfer_1-2d':    1071,
+        'Transfer_2-3d':    1072,
+        'Transfer_3-4d':    1073,
+        'Transfer_4-5d':    1074,
+        'Transfer_5-6d':    1081,
+        'Transfer_1-2c':    1278,
+        'Transfer_2-3c':    1279,
+        'Transfer_3-4c':    1280,
+        'Transfer_4-5c':    1281,
+        'Transfer_5-6c':    1282
     }
 
 if __name__ == '__main__':
@@ -23,26 +28,24 @@ if __name__ == '__main__':
 
     fig, axes = plt.subplots(3, 2)
     index = 0
-    firstTransfer = False
     eval = 0
     level = 1
 
     for sessionName in sessions.keys():
-        if not firstTransfer and sessionName.find('Transfer') is not -1:
-            firstTransfer = True
+        if sessionName.find('Transfer_1-2') is not -1:
             index = 1
             level = 2
 
         name = sessionName if eval is 0 else sessionName + '_eval'
-        data = read_csv(f'../../data/{name}.csv')
+        data = read_csv(f'../../data/{name}_accum.csv')
 
         sns.lineplot(x='episode', y='reward', data=data, ax=axes[index // 2, index % 2])
 
         axes[index // 2, index % 2].set_title(f'Level {level}')
         axes[index // 2, index % 2].xaxis.set_major_locator(MultipleLocator((index+1) * 100))
         axes[index // 2, index % 2].xaxis.set_major_formatter(FormatStrFormatter('%d'))
-        axes[index // 2, index % 2].yaxis.set_major_locator(MultipleLocator(1000))
-        axes[index // 2, index % 2].yaxis.set_major_formatter(FormatStrFormatter('%d'))
+        # axes[index // 2, index % 2].yaxis.set_major_locator(MultipleLocator(1000))
+        # axes[index // 2, index % 2].yaxis.set_major_formatter(FormatStrFormatter('%d'))
         fig.show()
 
         index += 1
@@ -54,4 +57,4 @@ if __name__ == '__main__':
         ax.set_xlabel('')
 
     fig.show()
-    fig.savefig('../../data/BaseResults')
+    fig.savefig('../../data/BaseResultsAccum')
